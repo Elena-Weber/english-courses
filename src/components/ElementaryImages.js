794b56b4436dfa016897ff012db022fccdb9f178
Elena-Weber <crochetmiracles@gmail.com>
2023-06-11 19:29:42 -0400

    add react bootstrap

--- src/components/ElementaryImages.js
+++ src/components/ElementaryImages.js
@@ -12,19 +12,19 @@ import IMG_2946 from '../images/elementary/IMG_2946.jpg';
 
 const ElementaryImages = () => {
   return (
-    <div>
-      <h1>ElementaryImages</h1>
-      <img src={IMG_2922} alt="elem1" />;
-      <img src={IMG_2925} alt="elem1" />;
-      <img src={IMG_2926} alt="elem1" />;
-      <img src={IMG_2927} alt="elem1" />;
-      <img src={IMG_2929} alt="elem1" />;
-      <img src={IMG_2931} alt="elem1" />;
-      <img src={IMG_2932} alt="elem1" />;
-      <img src={IMG_2933} alt="elem1" />;
-      <img src={IMG_2934} alt="elem1" />;
-      <img src={IMG_2945} alt="elem1" />;
-      <img src={IMG_2946} alt="elem1" />;
+    <div className='text-center'>
+      <h4>Отзывы от тех, кто уже прошёл данный курс:</h4>
+      <img src={IMG_2922} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2925} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2926} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2927} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2929} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2931} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2932} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2933} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2934} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2945} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
+      <img src={IMG_2946} alt="elem1" style={{maxWidth: "100%"}} className="my-2" />
     </div>
   )
 }
---
+++ src/components/ElementaryIntro.js
@@ -0,0 +1,48 @@
+import Accordion from 'react-bootstrap/Accordion';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ElementaryTopics from './ElementaryTopics'
+
+function ElementaryIntro() {
+  return (
+    <Accordion flush>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>
+          <h5>Кому подойдёт данный курс?</h5>
+        </Accordion.Header>
+        <Accordion.Body>
+          <ListGroup as="ul" variant="flush" className="text-start">
+            <ListGroup.Item as="li">Тем, кто уже умеет читать на английском языке (даже если не всегда правильно)</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто когда-то уже изучал English (но не очень успешно)</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто любит информацию, логично разложенную по полочкам и без воды</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто хочет изучать язык в комплексе (произношение, грамматика, аудирование, лексика, разговор)</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто готов ударно потрудиться ради ощутимого результата</ListGroup.Item>
+          </ListGroup>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>
+          <h5>Кому курс не подойдёт?</h5>
+        </Accordion.Header>
+        <Accordion.Body>
+          <ListGroup as="ul" variant="flush" className="text-start">
+            <ListGroup.Item as="li">Тем, кто не умеет читать in English</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто уже имеет базовые знания по английскому языку</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто любит долгие разговоры ни о чём</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кому нужно подтянуть лишь один аспект языка (произношение, грамматика, аудирование, лексика, разговор)</ListGroup.Item>
+            <ListGroup.Item as="li">Тем, кто хочет комплексный результат за 2 часа</ListGroup.Item>
+          </ListGroup>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>
+          <h5>Что будет на этом курсе?</h5>
+        </Accordion.Header>
+        <Accordion.Body>
+          <ElementaryTopics/>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default ElementaryIntro;
