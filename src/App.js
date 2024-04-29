b60030b25f9c635bcf83c6ab65d86845bbb5d6b5
Elena-Weber <crochetmiracles@gmail.com>
2024-04-29 17:16:58 -0400

    update urls

--- src/App.js
+++ src/App.js
@@ -8,10 +8,40 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 export default function App() {
+  // delete or comment out these 3 blocks if not needed anymore
+  const url = window.location.href;
+  let key = "";
+
+  if (url.includes("elementary")) {
+    key = "elementary";
+  } else if (url.includes("pre-intermediate")) {
+    key = "pre-intermediate";
+  } else if (url.includes("techies")) {
+    key = "techies";
+  } else if (url.includes("alpha")) {
+    key = "alpha";
+  } else if (url.includes("home")){
+    key = "home";
+  } else {
+    key = "home";
+  };
+
+  const handleEventKey = (eventKey) => {
+    key = eventKey;
+  };
+
   return (
     <>
     <Heading/>
-    <Tabs defaultActiveKey="home" id="justify-tab" className="mb-3 mx-3 fs-5 navi-tabs" justify>
+    <Tabs
+      defaultActiveKey="home"
+      id="justify-tab"
+      className="mb-3 mx-3 fs-5 navi-tabs"
+      justify
+      // delete or comment out next 2 lines if not needed anymore
+      activeKey={key}
+      onSelect={(eventKey) => handleEventKey(eventKey)}
+    >
       <Tab eventKey="home" title="Home">
         <Home/>
       </Tab>
