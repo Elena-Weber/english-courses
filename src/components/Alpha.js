37ae51294523eb3989f8b59c3f32ec4d2eef179c
Elena-Weber <crochetmiracles@gmail.com>
2024-10-13 19:24:24 -0400

    october update

--- src/components/Alpha.js
+++ src/components/Alpha.js
@@ -33,8 +33,8 @@ const Alpha = () => {
               <li><span className='fw-bold'>Июнь 2024</span> - медицина и здоровье <span className="text-danger">(получите доступ сразу ко всем пройденным словам и заданиям)</span></li>
               <li><span className='fw-bold'>Июль 2024</span> - товары и шоппинг <span className="text-danger">(получите доступ сразу ко всем пройденным словам и заданиям)</span></li>
               <li><span className='fw-bold'>Август 2024</span> - продукты и питание <span className="text-danger">(получите доступ сразу ко всем пройденным словам и заданиям)</span></li>
-              <li><span className='fw-bold'>Сентябрь 2024</span> - путешествия и мир <span className="text-danger fw-bold">(начало занятий - 4 сентября)</span></li>
-              <li><span className='fw-bold'>Октябрь 2024</span> - сленг и идиомы</li>
+              <li><span className='fw-bold'>Сентябрь 2024</span> - путешествия и мир <span className="text-danger">(получите доступ сразу ко всем пройденным словам и заданиям)</span></li>
+              <li><span className='fw-bold'>Октябрь 2024</span> - американские сленг и идиомы <span className="text-danger">(получите доступ сразу ко всем пройденным словам и заданиям)</span></li>
             </div>
             <div>
               <AlphaPayment />
